7ec68cc15e4e1cd45f1f0ffcb8e2a0b86bfbfaa4
huuhoang990 <huuhoang990@gmail.com>
2024-07-18 06:17:55 +0700

    html and css

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,12 @@
 import { RouterProvider } from 'react-router-dom'
-import './App.css'
+import '@/assets/css/style.scss'
 import { router } from "@/Routes/Routes";
 
 function App() {
   return (
-    <>
-      <div>
+      <>
         <RouterProvider router={router} />
-      </div>
-    </>
+      </>
   )
 }
 
--- react-app/src/Layout/MainLayout.tsx
+++ react-app/src/Layout/MainLayout.tsx
@@ -2,8 +2,12 @@ import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
   return (
-    <div>
-      <Outlet></Outlet>
+    <div id="container">
+      <div id="content">
+        <div id="inner-content">
+          <Outlet></Outlet>
+        </div>
+      </div>
     </div>
   );
 }
