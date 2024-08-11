70dd9c425961c0c320de3f899bdb1a87568e7a8d
huuhoang990 <huuhoang990@gmail.com>
2024-08-11 07:20:38 +0700

    modify api

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -20,12 +20,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 */
 
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('profile', [AuthController::class, 'profile']);
+});
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('login', [AuthController::class, 'login']);
+
     /*
+     * Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('profile', 'AuthController@profile');
+    });
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        Route::get('user-info', 'UserController@getUserInfo');
+    });
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('profile', 'AuthController@profile');
